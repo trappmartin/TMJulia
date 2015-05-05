c36247d2cf88c1e907d455f7fdf819bfcc1ffe23
Renzo Poddighe <poddighe.renzo@gmail.com>
2015-05-05 12:13:22 +0200

    Add command-line options to LDA

--- lda.jl
+++ lda.jl
@@ -42,7 +42,7 @@ function sampleTopic(ndk, nkw, nk, document, word, K, W, alpha, beta)
 	p[1] = (ndk[document, 1] + alpha) * ((nkw[1, word] + beta)/(nk[1] + (W * beta)))
 	for k = 2:K 
 		p[k] = p[k-1] + (ndk[document, k] + alpha) * ((nkw[k, word] + beta)/(nk[k] + (W * beta)))
-	end
+    end
 	u = p[K]*rand()
 	for topic in 1:K
 		if u < p[topic]
@@ -55,7 +55,6 @@ function gibbs(dictionary, corpus, topics, ndk, nkw, nk, K, alpha, beta, iterati
 	W = length(dictionary)
 	for n = 1:iterations
 		println("Iteration $n")
-		println("Topic counts: ", nk)
 		for (i, doc) in enumerate(corpus)
 
 			for (j, word) in enumerate(doc)
@@ -97,20 +96,43 @@ function estimatePhi(nkw, nk, beta, dictionary)
 	phikw
 end
 
-function printTopics(phi, dictionary, nwords)
-	println([dictionary phi'])
+function printTopics(phi, dictionary, nwords, K)
+    for k = 1:K
+        topic = hcat(phi'[:,k], dictionary)
+        println("Topic $k:\n $(sortrows(topic, rev=true, by=x->(x[1]))[1:nwords,:])")
+    end
 end
 
-function exampleRun()
-	(dictionary, corpus) = parseCorpus("data/vocab.txt", "data/ap.dat")
-	alpha = 1
-	beta = 0.1
-	K = 5
-	iterations = 100
-	Z, ndk, nkw, nk = gibbs(dictionary, corpus, initTopics(dictionary, corpus, 5)..., K, alpha, beta, iterations)
-	printTopics(estimatePhi(nkw, nk, beta, dictionary), dictionary, 10)
-
-	nothing
+using ArgParse
+s = ArgParseSettings()
+@add_arg_table s begin
+    "--num_topics", "-k"
+    help = "The number of topics"
+    arg_type = Int
+    required = true
+    "--alpha", "-a"
+    help = "The alpha hyperparameter"
+    arg_type = Number
+    default = 1
+    "--beta", "-b"
+    help = "The beta hyperparameter"
+    arg_type = Number
+    default = 0.01
+    "--iterations", "-i"
+    help = "The number of iterations"
+    arg_type = Int
+    required = true
+    "--dictionary", "-d"
+    help = "Path to vocabulary file"
+    "corpus"
+    help = "Path to corpus file"
+    required = true
 end
 
-exampleRun()
+parsed_args = parse_args(s)
+println("Running LDA with the following settings:\n$parsed_args")
+(dictionary, corpus) = parseCorpus(parsed_args["dictionary"], parsed_args["corpus"])
+Z, ndk, nkw, nk = gibbs(dictionary, corpus, initTopics(dictionary, corpus, parsed_args["num_topics"])..., parsed_args["num_topics"], parsed_args["alpha"], parsed_args["beta"], parsed_args["iterations"])
+
+printTopics(estimatePhi(nkw, nk, parsed_args["beta"], dictionary), dictionary, 10, parsed_args["num_topics"])
+
