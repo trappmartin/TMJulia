5152148e85a547b80a25cd48775ae6360ee176ae
Renzo Poddighe <poddighe.renzo@gmail.com>
2015-06-18 12:02:23 +0200

    Setup btm for experimentsa

--- btm.jl
+++ btm.jl
@@ -25,7 +25,7 @@ function parseCorpus(dpath, cpath, window)
 	dictionary, biterms
 end
 
-function initialize(dictionary::Array{ASCIIString, 1}, biterms::Array{(Int64, Int64), 1}, K::Int64)
+function initialize(dictionary::Array{String, 1}, biterms::Array{(Int64, Int64), 1}, K::Int64)
 	topics = Array(Int64, length(biterms))
 	nk = zeros(Int64, K)
 	nwz = zeros(Int64, length(dictionary), K)
@@ -53,47 +53,61 @@ function sampleTopic(nk::Array{Int64,1}, nwz::Array{Int64,2}, w1::Int64, w2::Int
 	end
 end
 
-function gibbs(dictionary::Array{ASCIIString, 1}, biterms::Array{(Int64, Int64), 1}, topics::Array{Int64,1}, nk::Array{Int64,1}, nwz::Array{Int64,2}, K::Int64, alpha::Number, beta::Number, iterations::Int64)
+function gibbs(dictionary::Array{String, 1}, biterms::Array{(Int64, Int64), 1}, topics::Array{Int64,1}, nk::Array{Int64,1}, nwz::Array{Int64,2}, K::Int64, alpha::Number, beta::Number, iterations::Int64)
 	W = length(dictionary)
 	for n = 1:iterations
 		println("Iteration $n")
-        for (i, biterm) in enumerate(biterms)
-            w1 = biterm[1]
-            w2 = biterm[2]
-			topic = topics[i]
-			nk[topic] -= 1
-			nwz[w1, topic] -= 1
-			nwz[w2, topic] -= 1
+      for (i, biterm) = enumerate(biterms)
+          w1 = biterm[1]
+          w2 = biterm[2]
+			    topic = topics[i]
+			    nk[topic] -= 1
+			    nwz[w1, topic] -= 1
+			    nwz[w2, topic] -= 1
 
-			topic = sampleTopic(nk, nwz, w1, w2, K, W, alpha, beta)
+			    topic = sampleTopic(nk, nwz, w1, w2, K, W, alpha, beta)
 
-			topics[i] = topic
-			nk[topic] += 1
-			nwz[w1, topic] += 1
-			nwz[w2, topic] += 1
-		end
+			    topics[i] = topic
+			    nk[topic] += 1
+			    nwz[w1, topic] += 1
+			    nwz[w2, topic] += 1
+		  end
 	end
-	topics, nk, nwz
+	  topics, nk, nwz
 end
 
-function estimateTheta(ndk, alpha, K)
-	thetadk = Array(Float64, size(ndk)...)
-	for d in 1:size(thetadk, 1)
-		for k in 1:size(thetadk, 2)
-			thetadk[d,k] = (ndk[d,k] + alpha) / (sum(ndk, 2)[d] + (K * alpha))
-		end
-	end
-	thetadk
+function estimateTheta(nz, biterms, alpha, K)
+	  thetaz = Array(Float64, length(nz))
+	  for z in 1:length(thetaz)
+        thetaz[z] = (nz[z] + alpha) / (length(biterms) + (K * alpha))
+	  end
+	  thetaz
 end
 
 function estimatePhi(nkw, nk, beta, dictionary)
     phikw = Array(Float64, size(nkw)...)
-	for k in 1:size(phikw, 1)
-		for w in 1:size(phikw, 2)
-			phikw[k,w] = (nkw[k,w] + beta)/(nk[k] + (length(dictionary) * beta))
-		end
-	end
-	phikw
+	  for k in 1:size(phikw, 1)
+		    for w in 1:size(phikw, 2)
+			      phikw[k,w] = (nkw[k,w] + beta)/(nk[k] + (length(dictionary) * beta))
+		    end
+	  end
+	  phikw
+end
+
+function perplexity(dictionary, biterms, phi, theta)
+    logprob_b = 0
+    for (b, biterm) in enumerate(biterms)
+        logprob_1 = 0
+        logprob_2 = 0
+
+        logprob_1 += log(sum(vec(phi[:,biterm[1]]) .* vec(theta)))
+        logprob_2 += log(sum(vec(phi[:,biterm[2]]) .* vec(theta)))
+        logprob_b = logprob_1 + logprob_2
+    end
+    println(logprob_b)
+    logprob_b /= sum(map(length, biterms))
+    println(logprob_b)
+    exp(-1 * logprob_b)
 end
 
 function printTopics(phi, dictionary, nwords, K)
@@ -103,42 +117,53 @@ function printTopics(phi, dictionary, nwords, K)
     end
 end
 
-using ArgParse
-s = ArgParseSettings()
-@add_arg_table s begin
-    "--num_topics", "-k"
-    help = "The number of topics"
-    arg_type = Int
-    required = true
-    "--alpha", "-a"
-    help = "The alpha hyperparameter"
-    arg_type = Number
-    default = 1
-    "--beta", "-b"
-    help = "The beta hyperparameter"
-    arg_type = Number
-    default = 0.01
-    "--iterations", "-i"
-    help = "The number of iterations"
-    arg_type = Int
-    required = true
-    "--dictionary", "-d"
-    help = "Path to vocabulary file"
-    "corpus"
-    help = "Path to corpus file"
-    required = true
-    "--window", "-w"
-    help = "The context window in which co-occurrences are considered"
-    arg_type = Int
-    default = 15
+using HDF5, JLD
+
+(dictionary, biterms) = parseCorpus("data/reuters_vocab.dat", "data/reuters_train.dat", 15)
+for K = 130:10:200
+    println("Running BTM for ", K, " topics")
+    (Z, nk, nwz) = initialize(dictionary, biterms, K)
+    (Z, nk, nwz) = gibbs(dictionary, biterms, Z, nk, nwz, K, 1, 0.1, 350)
+    save(string("models/btm/btm_1_01_", K), "Z", Z, "nk", nk, "nwz", nwz)
 end
 
-parsed_args = parse_args(s)
-println("Running BTM with the following settings:\n$parsed_args")
-(dictionary, biterms) = parseCorpus(parsed_args["dictionary"], parsed_args["corpus"], parsed_args["window"])
-Z, nk, nwz = initialize(dictionary, biterms, parsed_args["num_topics"])
-gibbs(dictionary, biterms, Z, nk, nwz, parsed_args["num_topics"], parsed_args["alpha"], parsed_args["beta"], parsed_args["iterations"])
 
-printTopics(estimatePhi(nwz', nk, parsed_args["beta"], dictionary), dictionary, 10, parsed_args["num_topics"])
+# using ArgParse
+# s = ArgParseSettings()
+# @add_arg_table s begin
+#     "--num_topics", "-k"
+#     help = "The number of topics"
+#     arg_type = Int
+#     required = true
+#     "--alpha", "-a"
+#     help = "The alpha hyperparameter"
+#     arg_type = Number
+#     default = 1
+#     "--beta", "-b"
+#     help = "The beta hyperparameter"
+#     arg_type = Number
+#     default = 0.01
+#     "--iterations", "-i"
+#     help = "The number of iterations"
+#     arg_type = Int
+#     required = true
+#     "--dictionary", "-d"
+#     help = "Path to vocabulary file"
+#     "corpus"
+#     help = "Path to corpus file"
+#     required = true
+#     "--window", "-w"
+#     help = "The context window in which co-occurrences are considered"
+#     arg_type = Int
+#     default = 15
+# end
 
+# parsed_args = parse_args(s)
+# println("Running BTM with the following settings:\n$parsed_args")
+# (dictionary, biterms) = parseCorpus(parsed_args["dictionary"], parsed_args["corpus"], parsed_args["window"])
+# Z, nk, nwz = initialize(dictionary, biterms, parsed_args["num_topics"])
+# gibbs(dictionary, biterms, Z, nk, nwz, parsed_args["num_topics"], parsed_args["alpha"], parsed_args["beta"], parsed_args["iterations"])
 
+# printTopics(estimatePhi(nwz', nk, parsed_args["beta"], dictionary), dictionary, 10, parsed_args["num_topics"])
+
+# println(perplexity(dictionary, biterms, estimatePhi(nwz', nk, parsed_args["beta"], dictionary), estimateTheta(sum(nwz, 1), biterms, parsed_args["alpha"], parsed_args["num_topics"])))
